f128f5924989a7421801cd00409aebc3f723ebbb
gksmsla92 <gksmsla92@naver.com>
2023-07-04 09:28:00 +0900

    test

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,12 +1,34 @@
-function slideShow() {
-  const images = document.querySelectorAll('.slideshow img');
-  let currentIndex = 0;
+// JavaScript 코드
+const slideshow = document.querySelector('.slideshow');
+const container = slideshow.querySelector('.slideshow-container');
+const items = container.querySelectorAll('.slideshow-item');
+const prevBtn = slideshow.querySelector('.slideshow-prev');
+const nextBtn = slideshow.querySelector('.slideshow-next');
+let currentIndex = 0;
+let intervalId;
 
-  setInterval(() => {
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % images.length;
-    images[currentIndex].classList.add('active');
-  }, 2000); // 이미지가 2초마다 넘어감 (여기서 2000은 밀리초 단위로 지정합니다.)
+// 이전 슬라이드로 이동
+function prevSlide() {
+  items[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  items[currentIndex].classList.add('active');
 }
 
-slideShow();
+// 다음 슬라이드로 이동
+function nextSlide() {
+  items[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % items.length;
+  items[currentIndex].classList.add('active');
+}
+
+// 슬라이드를 자동으로 이동시키는 함수
+function startSlideShow() {
+  intervalId = setInterval(nextSlide, 2000);
+}
+
+// 이전/다음 버튼 이벤트 처리
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+// 슬라이드 쇼 시작
+startSlideShow();
